adb2ac5075c9ffacd68964709ff7b905eb8f6bdb
Sean Dwyer <spdwyer06@gmail.com>
2021-06-04 14:25:22 -0400

    Add db context and set up connection string

---
+++ RestaurantRater_Web/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater_Web.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
